1dd5aeec5f74c3342b76055ce2a7078278343150
Stefan Benz <stefan@caos.ch>
2020-09-18 11:06:14 +0200

    fix(documentation): changed workflow structure

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,17 @@
 module.exports = {
-    plugins: [ 
+    branches: ["master"],
+    plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
         ["@semantic-release/github", {
             "assets": [
-              {"path": "./artifacts/documentation", "label": "Documentation"},
+                {"path": ".artifacts/documentation-darwin-amd64/documentation-darwin-amd64", "label": "Darwin x86_64"},
+                {"path": ".artifacts/documentation-linux-amd64/documentation-linux-amd64", "label": "Linux x86_64"},
+                {
+                    "path": ".artifacts/documentation-windows-amd64/documentation-windows-amd64",
+                    "label": "Windows x86_64"
+                },
             ]
-        }]
+        }],
     ]
-  };
+};
